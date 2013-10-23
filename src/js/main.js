bc33694b21d439dd6d9cb29cbf10e39b861715a5
Andrew Davis <theandrewdavis@gmail.com>
2013-10-23 08:44:35 -0400

    Center canvas in browser window

--- src/js/main.js
+++ src/js/main.js
@@ -1,29 +1,25 @@
 var app = {};
 
-app.setup = function () {
-    app.renderer = new PIXI.autoDetectRenderer(window.innerWidth, window.innerHeight);
-    app.background = new PIXI.Stage;
-    app.whale = new PIXI.Sprite(PIXI.Texture.fromImage('/img/whale.png'));
+app.setupStage = function () {
+    app.renderer = new PIXI.autoDetectRenderer(800, 600, $('#game')[0]);
+    app.background = new PIXI.Stage(0x66FF99);
+    app.whale = new PIXI.Sprite(PIXI.Texture.fromImage('img/whale.png'));
 
     app.whale.position.x = window.innerWidth / 2;
     app.whale.position.y = window.innerHeight / 2;
     app.whale.rotation 
     app.background.addChild(app.whale);
-
-    document.body.appendChild(app.renderer.view);
 };
 
-app.animate = function () {
+
+
+app.update = function () {
     app.whale.rotation += 0.01;
     app.renderer.render(app.background);
-    requestAnimationFrame(app.animate);
-};
-
-app.run = function () {
-    requestAnimationFrame(app.animate);
+    window.requestAnimationFrame(app.update);
 };
 
 $(function () {
-    app.setup();
-    app.run();
+    app.setupStage();
+    window.requestAnimationFrame(app.update);
 });
