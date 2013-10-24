de41f1fa714d72ba1f829d1ea8382ec3db1174b4
Andrew Davis <theandrewdavis@gmail.com>
2013-10-24 08:17:16 -0400

    Proof of concept kepresses

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,29 @@
 var app = {};
 
+app.keyName = function (event) {
+    return jQuery.hotkeys.specialKeys[event.which] || String.fromCharCode(event.which).toLowerCase();
+};
+
+app.setupKeys = function () {
+    app.keys = {};
+    $(document).bind("keydown", function(event) {
+        app.keys[app.keyName(event)] = true;
+    });
+    $(document).bind("keyup", function(event) {
+        app.keys[app.keyName(event)] = false;
+    });
+};
+
+$(function() {
+  window.keydown = {};
+  
+  function keyName(event) {
+    return jQuery.hotkeys.specialKeys[event.which] ||
+      String.fromCharCode(event.which).toLowerCase();
+  }
+  
+});
+
 app.setupStage = function () {
     app.renderer = new PIXI.autoDetectRenderer(800, 600, $('#game')[0]);
     app.background = new PIXI.Stage(0x66FF99);
@@ -11,15 +35,18 @@ app.setupStage = function () {
     app.background.addChild(app.whale);
 };
 
-
-
 app.update = function () {
     app.whale.rotation += 0.01;
     app.renderer.render(app.background);
     window.requestAnimationFrame(app.update);
+
+    if (app.keys['a']) {
+        console.log('A');
+    }
 };
 
 $(function () {
+    app.setupKeys();
     app.setupStage();
     window.requestAnimationFrame(app.update);
 });
